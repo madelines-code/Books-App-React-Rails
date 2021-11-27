d4e598eb84fc00b35e8ce183deecbf2332dd3479
Madeline Armstrong <armstrong.madeline@gmail.com>
2021-11-27 15:50:53 -0700

    seeded data, created routes and controller methods

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,20 +1,51 @@
 class BooksController < ApplicationController
+
+  before_action :set_item, only: [:show, :update, :destroy]
   # this remders the component
-def app 
-  render component: "App"
-end
+  def app 
+    render component: "App"
+  end
 
- # these are all json
-def index 
-  render json: Item.all
-end
- 
-def show
-end
+  # these are all json
+  def index 
+    render json: Book.all
+  end
+  
+  def show
+    render json: @book 
+  end
 
-def update 
-end
+  def create
+  @book = Book.new(book_params)
+  if(@book.save)
+    render json: @book
+  else
+    # 422 error = unprocessable entity (doesn't clear the sanitization params)
+    render json: {errors: @book.errors.full_messages}, status: 422
+  end
+  end
 
-def destroy
-end
+
+  def update 
+    if(@book.update(book_params))
+      render json: @book
+    else
+      render json: { errors: @book.errors.full_messages }, status: 422 
+  end
+
+  def destroy
+    render json: @book.destroy 
+  end
+
+  private
+  def set_book
+    @book=Book.find(params[:id])
+  end
+
+  #the reason we sanitize our data is to keep out database clean with only valid entries
+  def book_params
+    Book.require(:book).permit(:title, :author)
+  end
+
+
 end
